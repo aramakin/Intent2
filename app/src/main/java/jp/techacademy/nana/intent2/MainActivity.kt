c948cd7638fa524e7dadd3565eadc4ff01156fa5
aramakin <aramakin@cocoto.co.jp>
2020-10-19 15:42:19 +0900

    Intent2のエラーが出るコードです

---
+++ app/src/main/java/jp/techacademy/nana/intent2/MainActivity.kt
@@ -0,0 +1,22 @@
+package jp.techacademy.nana.intent2
+
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_TEXT, editText.text.toString())
+            startActivity(intent)
+        }
+    }
+}
